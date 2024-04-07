da28779e9293f875bcba6016b863ecd0d6ae4d47
kerelito <k.lempicki.25@gmail.com>
2024-04-07 13:24:29 +0200

    Zmiany

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/klemp/CLionProjects/PO-PWr-TEL-HW2
+# Install script for directory: /Users/karol/Git/PO-PWr-TEL-HW2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/PO_PWr_TEL_HW2")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "E:/CLion 2023.3.4/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/klemp/CLionProjects/PO-PWr-TEL-HW2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/karol/Git/PO-PWr-TEL-HW2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <map>
 using namespace std;
 
 class GameModel {
 private:
     int points;
+    int level;
 
 public:
-    GameModel() : points(0) {}
+    GameModel() : points(0), level(1) {}
 
     void addPoint() {
         points++;
     }
 
-    [[nodiscard]] int getPoints() const {
+    void setPoints(int newPoints) {
+        points = newPoints;
+    }
+
+    int getPoints() const {
         return points;
     }
 
-    [[nodiscard]] bool NextLevel() const {
+    void setLevel(int newLevel) {
+        level = newLevel;
+    }
+
+    int getLevel() const {
+        return level;
+    }
+
+    bool nextLevel() const {
         return (points % 5 == 0);
     }
 };
@@ -29,11 +44,12 @@ public:
         cout << "Uzytkowniku, aby naliczac kolejne punkty nalezy podac do wejscia litere 'b'." << endl;
         cout << "Punkty oraz poziomy beda naliczane automatycznie." << endl;
         cout << "Aby zakonczyc dzialanie programu nalezy wcisnac 'e'." << endl;
+        cout << "W kazdym momencie trwania programu można zapis progres - 's', bądź go wczytać - 'l'" << endl;
         cout << "Milego klikania!.\n" << endl;
     }
 
     static void printData(int points, int level) {
-        cout << "Level: " << romanNumeral(level) << endl;
+        cout << "Level: " << (level > 0 ? romanNumeral(level) : "I") << endl;
         cout << "Points: " << points << endl;
     }
 
@@ -106,27 +122,70 @@ class GameController {
 private:
     GameModel& model;
     GameView& view;
-    int level;
 
 public:
-    GameController(GameModel& model, GameView& view) : model(model), view(view), level(1) {}
+    GameController(GameModel& model, GameView& view) : model(model), view(view) {}
 
     void launch() {
         GameView::printStartHeader();
         while (true) {
-            view.printData(model.getPoints(), level);
+            view.printData(model.getPoints(), model.getLevel());
             char input = GameView::getUserInput();
-            if (input == 'b') {
+            const char addPointCondition = 'b';
+            const char exitCondition = 'e';
+            const char saveCondition = 's';
+            const char loadCondition = 'l';
+            if (input == addPointCondition) {
                 model.addPoint();
-                if (model.NextLevel()) {
-                    level++;
+                if (model.nextLevel()) {
+                    model.setLevel(model.getLevel() + 1);
                 }
-            } else if (input == 'e') {
+            } else if (input == exitCondition) {
                 cout << "Program zakonczony.\n";
                 break;
+            } else if (input == saveCondition) {
+                cout << "Podaj nazwe pliku do zapisu: ";
+                string filename;
+                cin >> filename;
+                saveGame(filename, model.getPoints(), model.getLevel());
+                cout << "Stan gry zapisany.\n";
+            } else if (input == loadCondition) {
+                cout << "Podaj nazwe pliku do wczytania: ";
+                string filename;
+                cin >> filename;
+                if (loadGame(filename, model)) {
+                    cout << "Stan gry wczytany.\n";
+                } else {
+                    cout << "Nie udalo sie wczytac stanu gry z pliku.\n";
+                }
             } else {
-                cout << "Wprowadzono nieobslugiwane wejscie. Akceptowane pola wyboru to 'b' oraz 'e'.\n";
+                cout << "Wprowadzono nieobslugiwane wejscie. Akceptowane pola wyboru to 'b', 'e', 's' oraz 'l'.\n";
             }
+        }
+    }
+
+    void saveGame(const string& filename, int points, int level) {
+        ofstream file(filename);
+        if (file.is_open()) {
+            file << points << endl;
+            file << level << endl;
+            file.close();
+        } else {
+            cout << "Nie udalo sie zapisac stanu gry do pliku.\n";
+        }
+    }
+
+    bool loadGame(const string& filename, GameModel& model) {
+        ifstream file(filename);
+        if (file.is_open()) {
+            int points, level;
+            file >> points >> level;
+            model.setPoints(points);
+            model.setLevel(level);
+            file.close();
+            return true;
+        } else {
+            return false;
         }
     }
 };
@@ -135,6 +194,21 @@ int main() {
     GameModel model;
     GameView view;
     GameController controller(model, view);
+
+    // Wczytaj stan gry z pliku, jeśli możliwe
+    char input;
+    cout << "Czy chcesz wczytac stan gry z pliku? (T/N): ";
+    cin >> input;
+    if (toupper(input) == 'T') {
+        cout << "Podaj nazwe pliku do wczytania: ";
+        string filename;
+        cin >> filename;
+        if (!controller.loadGame(filename, model)) {
+            cout << "Nie udalo sie wczytac stanu gry z pliku.\n";
+        }
+    } else {
+        cout << "Nowa gra rozpoczeta.\n";
+    }
 
     controller.launch();
 
